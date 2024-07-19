b393eb2e794d962892ce1d26c8a18a8a270d3030
Andrés Olivé <andresolive@gmail.com>
2024-07-19 17:26:17 -0600

    create user in db

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,29 @@
-const NewUser = () => {
-  return <div>new user</div>
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+
+  const match = await prisma.user.findUnique({
+    where: { clerkId: user.id as string },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+
+  redirect('/journal')
+}
+
+const NewUser = async () => {
+  await createNewUser()
+  return <div>...loading</div>
 }
 
 export default NewUser
