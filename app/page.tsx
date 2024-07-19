187670a449da9fe6f9f7b667b29b1241e1b0a940
Andrés Olivé <andresolive@gmail.com>
2024-07-19 17:25:55 -0600

    add conditional href

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link'
 
-export default function Home() {
+import { auth } from '@clerk/nextjs/server'
+
+export default async function Home() {
+  const { userId } = await auth()
+
+  let href = userId ? '/journal' : '/new-user'
+
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-black text-white">
       <div className="mx-auto w-full max-w-[600px]">
@@ -10,7 +16,7 @@ export default function Home() {
           you have to do is be honest.
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="rounded-lg bg-blue-600 px-4 py-2 text-xl">
               get started
             </button>
