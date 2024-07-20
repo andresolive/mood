cd84cbdb2dd68c368f32bcc15935b649d4fa3f2d
Andrés Olivé <andresolive@gmail.com>
2024-07-19 19:19:20 -0600

    add NewEntryCard and EntryCard in journal page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,8 @@
+import EntryCard from '@/components/EntryCard'
+import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import Link from 'next/link'
 
 prisma
 
@@ -20,7 +23,19 @@ const getEntries = async () => {
 const JournalPage = async () => {
   const entries = await getEntries()
 
-  return <div>Journal</div>
+  return (
+    <div className="h-full bg-zinc-400/10 p-10">
+      <h2 className="mb-8 text-3xl">Journal</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
+            <EntryCard entry={entry} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default JournalPage
