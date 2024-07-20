ae38386e6154d836ac684209519e185d0a3f9717
Andrés Olivé <andresolive@gmail.com>
2024-07-20 15:22:52 -0600

    add functionality to Editor component

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,7 +1,19 @@
 'use client'
 
+import { useState } from 'react'
+
 const Editor = ({ entry }) => {
-  return <div>{entry.content}</div>
+  const [value, setValue] = useState(entry.content)
+
+  return (
+    <div className="h-full w-full">
+      <textarea
+        className="h-full w-full p-8 text-xl outline-none"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      ></textarea>
+    </div>
+  )
 }
 
 export default Editor
