834f22f2fb7345bdc9f29d4da420707c1cc5d4b4
Andrés Olivé <andresolive@gmail.com>
2024-07-19 19:18:51 -0600

    fix styling

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,16 +3,16 @@ import { UserButton } from '@clerk/nextjs'
 const DashboardLayout = ({ children }) => {
   return (
     <div className="relative h-screen w-screen">
-      <aside className="absolute left-0 top-0 h-full w-[600px] border-r border-black/10">
+      <aside className="absolute left-0 top-0 h-full w-[200px] border-r border-black/10">
         Mood
       </aside>
-      <div className="ml-[200px]">
+      <div className="ml-[200px] h-full">
         <header className="h-[60px] border-b border-black/10">
           <div className="flex h-full w-full items-center justify-end px-6">
             <UserButton />
           </div>
         </header>
-        <div>{children}</div>
+        <div className="h-[calc(100vh-60px)]">{children}</div>
       </div>
     </div>
   )
