39e1c4df7854ddc1c7a04a26dccb3a4849b8e038
Andrés Olivé <andresolive@gmail.com>
2024-07-21 11:00:09 -0600

    add AI functionality to Question component

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,22 +1,32 @@
 'use client'
 
+import { askQuestion } from '@/utils/api'
 import { useState } from 'react'
 
 const Question = () => {
   const [value, setValue] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [response, setResponse] = useState()
 
   const onChange = (e) => {
     setValue(e.target.value)
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    setLoading(true)
+
+    const answer = await askQuestion(value)
+    setResponse(answer)
+    setValue('')
+    setLoading(false)
   }
 
   return (
     <div>
-      <form onSubmit={onChange}>
+      <form onSubmit={handleSubmit}>
         <input
+          disabled={loading}
           onChange={onChange}
           value={value}
           type="text"
@@ -24,12 +34,15 @@ const Question = () => {
           className="rounded-lg border border-black/20 px-4 py-2 text-lg"
         />
         <button
+          disabled={loading}
           type="submit"
           className="rounded-lg bg-blue-400 px-4 py-2 text-lg"
         >
           Ask
         </button>
       </form>
+      {loading && <div>Loading...</div>}
+      {response && <div>{response}</div>}
     </div>
   )
 }
