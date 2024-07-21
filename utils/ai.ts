0d8909560c25c71044b295a63b319b36befb34f5
Andrés Olivé <andresolive@gmail.com>
2024-07-21 10:45:40 -0600

    create qa util function

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,6 +1,9 @@
-import { OpenAI } from '@langchain/openai'
+import { OpenAI, OpenAIEmbeddings } from '@langchain/openai'
 import { PromptTemplate } from '@langchain/core/prompts'
+import { Document } from '@langchain/core/documents'
+import { loadQARefineChain } from 'langchain/chains'
 import { StructuredOutputParser } from 'langchain/output_parsers'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 import z from 'zod'
 
 const parser = StructuredOutputParser.fromZodSchema(
@@ -55,4 +58,31 @@ export const analyze = async (content) => {
   } catch (e) {
     console.log(e)
   }
+}
+
+export const qa = async (question, entries) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: {
+        id: entry.id,
+        createdAt: entry.createdAt,
+      },
+    })
+  })
+
+  const model = new OpenAI({
+    temperature: 0,
+    modelName: 'gpt-3.5-turbo',
+  })
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+  const res = await chain.invoke({
+    input_documents: relevantDocs,
+    question,
+  })
+
+  return res.output_text
 }
