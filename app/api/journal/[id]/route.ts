43617f36d7c23e59b532876b6e98254e146cc6f7
Andrés Olivé <andresolive@gmail.com>
2024-07-20 22:02:12 -0600

    generate analysis in patch function

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { NextResponse } from 'next/server'
@@ -16,6 +17,21 @@ export const PATCH = async (request: Request, { params }) => {
       content,
     },
   })
+
+  const _analysis = await analyze(updatedEntry.content)
+
+  const updated = await prisma.analysis.upsert({
+    where: {
+      entryId: updatedEntry.id,
+    },
+    create: {
+      entryId: updatedEntry.id,
+      ..._analysis,
+    },
+    update: _analysis,
+  })
+
+  console.log(updated)
 
   return NextResponse.json({ data: updatedEntry })
 }
